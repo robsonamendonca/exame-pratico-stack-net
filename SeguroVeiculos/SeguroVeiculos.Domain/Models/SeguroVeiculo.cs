6e9a8b665607e8e1156efd882b7a3b1886cd5ff9
Robson Mendonça <rob.ams@gmail.com>
2023-10-26 11:05:46 -0300

    referencias e dump mysql

---
+++ SeguroVeiculos/SeguroVeiculos.Domain/Models/SeguroVeiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeguroVeiculos.Domain.Models
+{
+    public class SeguroVeiculo
+    {
+        public int Id { get; set; }
+        public int SeguradoId { get; set; }
+        public int VeiculoId { get; set; }
+
+        public decimal ValorSeguro { get; set; }
+    }
+}
---
+++ dump.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Segurado (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(255),
+    CPF VARCHAR(11),
+    Idade INT
+);
+
+CREATE TABLE Veiculo (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    ValorVeiculo DECIMAL(10, 2),
+    MarcaModeloVeiculo VARCHAR(255)
+);
+
+CREATE TABLE SeguroVeiculo (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    SeguradoId INT,
+    VeiculoId INT,
+    ValorSeguro DECIMAL(10, 2),
+    FOREIGN KEY (SeguradoId) REFERENCES Segurado(Id),
+    FOREIGN KEY (VeiculoId) REFERENCES Veiculo(Id)
+);
